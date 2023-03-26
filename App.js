736ad2fdf5c0968259bbc84b6991219a6db0a4c3
danlaanh202 <mushroomlazy259@gmail.com>
2023-03-27 04:14:28 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,51 @@
+import "react-native-gesture-handler";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import UserScreen from "./screens/UserScreen";
+import { Ionicons } from "@expo/vector-icons";
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#3c0a6b",
+          },
+          headerTintColor: "white",
+          drawerActiveTintColor: "#3c0a6b",
+          drawerActiveBackgroundColor: "#f0e1ff",
+          // drawerStyle: {
+          //   backgroundColor: "#ccc",
+          // },
+        }}
+        initialRouteName="Welcome"
+      >
+        <Drawer.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            drawerIcon: ({ color }) => (
+              <Ionicons name="home" color={color} size={22} />
+            ),
+            drawerLabel: "Welcome Screen",
+          }}
+        />
+        <Drawer.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color} size={size} />
+            ),
+          }}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({});
